d3b42429a5fb447c5abfba4eb43a5a2278b89f17
Yee-Kun Jung <twoketchupplz@gmail.com>
2021-02-14 17:13:14 +0900

    CCG 2D
    파일 옮김

---
+++ CCG2D/Assets/CardDataBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDataBase : MonoBehaviour
+{
+    public static List<Card> cardList = new List<Card>();
+
+    void Awake()
+    {
+        cardList.Add(new Card(0, "None", 0, 0, "None"));
+        cardList.Add(new Card(0, "Elf", 0, 0, "It's Elf"));
+        cardList.Add(new Card(0, "Dwarf", 0, 0, "It's Dwarf"));
+        cardList.Add(new Card(0, "Doggaebee", 0, 0, "It's Doggaebee"));
+        cardList.Add(new Card(0, "Demon", 0, 0, "It's Demon"));
+    }
+}
