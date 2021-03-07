372f1e6785a2624d06a966a1d27bee29201b0c05
Yee-Kun Jung <twoketchupplz@gmail.com>
2021-03-07 23:07:40 +0900

    Attach Drag Handler
    IDragHandler, IBeginDragHandler, IEndDragHandler를 추가했다. Card를 UI로 변경했다. https://stackoverflow.com/questions/38548941/does-idraghandler-only-works-on-canvas-and-ui-items 참조

--- 2dDemoGame/Assets/Scripts/Card.cs
+++ 2dDemoGame/Assets/Scripts/Card.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Card : MonoBehaviour
+public class Card : MonoBehaviour, IBeginDragHandler ,IDragHandler, IEndDragHandler
 {
+    Vector2 currentPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -15,4 +18,22 @@ public class Card : MonoBehaviour
     {
         
     }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Drag");
+        currentPosition = Input.mousePosition;
+        transform.position = currentPosition;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End");
+    }
 }
+
+
